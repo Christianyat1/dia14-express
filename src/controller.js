52dd8be6a5e757eb48ddd872c2fe4839f4079aba
Christianyat1 <christianyatcaal123@gmail.com>
2024-04-19 13:43:11 -0600

    {caracteristica}: Archivo contoller.js y logica de ruta

---
+++ src/controller.js
@@ -0,0 +1,3 @@
+export const index = (req, res) => res.send('<h1>Bienvenido a mi API</h1>')
+export const getProducts = (req, res) => res.send('Mostrando productos en formato json...')
+export const getProductsByID = (req, res) => { res.send('Mostrando rpducto con el id...')})
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import express from 'express'
+import { getProducts, getProductsByID, index } from './controller.js'
 
 const app = express()
 app.get('/', (req, res) => res.send('<h1>Bienvenido a mi API</h1>'))
 
-app.get('/products', (req, res) => res.send('Mostrando productos en formato json...'))
+app.get('/products', getProducts())
 
-app.get('/products/:id', (req, res) => {
-  res.send('Mostrando rpducto con el id...')
-})
+app.get('/products/:id', getProductsByID())
 
 app.listen(3000, () => console.log('servidor ejecutandose en http://localhost:3000'))
+  
