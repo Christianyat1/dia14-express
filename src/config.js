4384e3ff94e7442058d54a3318d446b6da630efe
Christianyat1 <christianyatcaal123@gmail.com>
2024-04-19 13:30:42 -0600

    new changes

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@ config()
 export const DB_USER = process.env.DB_USER || 'root'
 export const DB_PASSWORD = process.env.DB_PASSWORD || ''
 export const DB_HOST = process.env.DB_HOST || 'localhost'
-export const DB_DATABASE = process.env.DB_DATABASE || 'art_gallery'
+export const DB_DATABASE = process.env.DB_DATABASE || 'bike'
 export const PORT = process.env.PORT || 3000
