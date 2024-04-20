df312d07fb42d6a3bd9104fc458bffde39555fa8
Christianyat1 <christianyatcaal123@gmail.com>
2024-04-20 08:41:18 -0600

    {ruta}: /products/:id termindado

--- src/controller.js
+++ src/controller.js
@@ -9,6 +9,28 @@ export const getProducts = async (req, res) => {
     res.status(500).json({ message: 'Hubo un error interno', detail: error.message })
   }
 }
-export const getProductsByID = (req, res) => {
-  res.send('Mostrando rpducto con el id...')
+export const getProductsByID = async (req, res) => {
+  try {
+    const { id } = req.params
+
+    // Validar que el parametro id sea un numero
+    const idNumber = parseInt(id)
+
+    console.log(idNumber)
+
+    if (isNaN(idNumber)) {
+      return res.status(400).json({ message: 'El id debe ser un numero' })
+    }
+
+    // Traer el producto de la base de datos
+    const [producto] = await pool.execute('SELECT * FROM product WHERE product_id=?', [idNumber])
+
+    // validar que exista el producto
+    if (producto.length === 0) {
+      return res.status(404).json({ message: 'Producto no encontrado ' })
+    }
+    res.json(producto[0])
+  } catch (error) {
+    res.status(500).json({ message: 'Hubo un error interno', detail: error.message })
+  }
 }
