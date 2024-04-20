42d51618ea89f957d76fbf57f211fb285c959b4c
Christianyat1 <christianyatcaal123@gmail.com>
2024-04-20 07:54:53 -0600

    {ruta}: /products terminado

--- src/controller.js
+++ src/controller.js
@@ -1,3 +1,14 @@
+import { pool } from './db.js'
+
 export const index = (req, res) => res.send('<h1>Bienvenido a mi API</h1>')
-export const getProducts = (req, res) => res.send('Mostrando productos en formato json...')
-export const getProductsByID = (req, res) => { res.send('Mostrando rpducto con el id...')})
+export const getProducts = async (req, res) => {
+  try {
+    const [productos] = await pool.query('SELECT * FROM product LIMIT 100')
+    res.json(productos)
+  } catch (error) {
+    res.status(500).json({ message: 'Hubo un error interno', detail: error.message })
+  }
+}
+export const getProductsByID = (req, res) => {
+  res.send('Mostrando rpducto con el id...')
+}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import express from 'express'
 import { getProducts, getProductsByID, index } from './controller.js'
 
 const app = express()
-app.get('/', (req, res) => res.send('<h1>Bienvenido a mi API</h1>'))
 
-app.get('/products', getProducts())
-
-app.get('/products/:id', getProductsByID())
+app.get('/', index)
+app.get('/products', getProducts)
+app.get('/products/:id', getProductsByID)
 
 app.listen(3000, () => console.log('servidor ejecutandose en http://localhost:3000'))
-  
